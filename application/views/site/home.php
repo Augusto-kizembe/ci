f41b777fac8f392117541879a8ad3814dcf54bfb
Bernardo Issenguel <beissenguel12@gmail.com>
2021-07-27 10:39:13 +0100

    adição de novas imagens da home

--- application/views/site/home.php
+++ application/views/site/home.php
@@ -4,33 +4,33 @@
     <div class="container">
       <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
         <div class="col-md-7 ftco-animate">
-          <span class="subheading"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_bemvido'):" Bem-vindo a Eduiba Projectos"; ?></span>
-          <h1 class="mb-4"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_principal'):" Somos a melhor solução para apostar"; ?></h1>
-          <p><a href="<? base_url() ?>servicos" class="btn btn-primary px-4 py-3 mt-3"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_btnServico'):" Nossos Serviços"; ?></a></p>
+          <span class="subheading"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_bemvido') : " Bem-vindo a Eduiba Projectos"; ?></span>
+          <h1 class="mb-4"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_principal') : " Somos a melhor solução para apostar"; ?></h1>
+          <p><a href="<? base_url() ?>servicos" class="btn btn-primary px-4 py-3 mt-3"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_btnServico') : " Nossos Serviços"; ?></a></p>
         </div>
       </div>
     </div>
   </div>
-  <div class="slider-item" style="background-image:url('<?= base_url() ?>public/images/home.jpeg');">
+  <div class="slider-item" style="background-image:url('<?= base_url() ?>public/images/home2.jpeg');">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
         <div class="col-md-7 ftco-animate">
-          <span class="subheading"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_bemvido'):" Bem-vindo a Eduiba Projectos"; ?></span>
-          <h1 class="mb-4"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_principal'):" Somos a melhor solução para apostar"; ?></h1>
-          <p><a href="<? base_url() ?>servicos" class="btn btn-primary px-4 py-3 mt-3"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_btnServico'):" Nossos Serviços"; ?></a></p>
+          <span class="subheading"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_bemvido') : " Bem-vindo a Eduiba Projectos"; ?></span>
+          <h1 class="mb-4"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_principal') : " Somos a melhor solução para apostar"; ?></h1>
+          <p><a href="<? base_url() ?>servicos" class="btn btn-primary px-4 py-3 mt-3"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_btnServico') : " Nossos Serviços"; ?></a></p>
         </div>
       </div>
     </div>
   </div>
-  <div class="slider-item" style="background-image:url('<?= base_url() ?>public/images/home.jpeg');">
+  <div class="slider-item" style="background-image:url('<?= base_url() ?>public/images/home1.jpeg');">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text align-items-center justify-content-start" data-scrollax-parent="true">
         <div class="col-md-7 ftco-animate">
-          <span class="subheading"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_bemvido'):" Bem-vindo a Eduiba Projectos"; ?></span>
-          <h1 class="mb-4"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_principal'):" Somos a melhor solução para apostar"; ?></h1>
-          <p><a href="<? base_url() ?>servicos" class="btn btn-primary px-4 py-3 mt-3"><?= ($this->session->userdata('idioma') != NULL)?lang('conteudo_btnServico'):" Nossos Serviços"; ?></a></p>
+          <span class="subheading"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_bemvido') : " Bem-vindo a Eduiba Projectos"; ?></span>
+          <h1 class="mb-4"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_principal') : " Somos a melhor solução para apostar"; ?></h1>
+          <p><a href="<? base_url() ?>servicos" class="btn btn-primary px-4 py-3 mt-3"><?= ($this->session->userdata('idioma') != NULL) ? lang('conteudo_btnServico') : " Nossos Serviços"; ?></a></p>
         </div>
       </div>
     </div>
